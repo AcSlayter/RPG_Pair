ea70c72a9ee6ccbfed3007b789b409ddf8110535
Aaron <aaronbcain@gmial.com>
2018-08-27 23:14:13 -0700

    boiler plate not needed yet with only jquery

--- Static/js/boilerplate.js
+++
@@ -1,8 +0,0 @@
-//Filename: boilerplate.js
-
- // configured in our bootstrap(main.js)
-define(['jquery'], function($){
-  // Above we have passed in jQuery
-  // They will not be accessible in the global scope
-  return {}; // What we return here will be used by other modules
-});
