f2c4de5b28f047433a9870e1506252a547997b2f
Beau Dekker <dekkerb115@gmail.com>
2018-08-31 17:59:35 -0700

    Added directional animation,collision, and simultaneous movement

--- Static/Beau/scripts/main.js
+++ Static/Beau/scripts/main.js
@@ -1,13 +1,28 @@
 frameRate = 50
 bgHeight = 500
 bgWidth = 500
-
-
-define("main",["Sprite","Spritesheet"],function (Sprite,Spritesheet){
-	spritesheet = new Spritesheet(150,120,"spritesheets/example3.png")
-	testSprite = new Sprite("sprite-container-1",spritesheet)
-	testSprite.handleKeyPress()
+sprites = []
+require.config({
+	baseUrl: 'scripts',
+	paths: {
+		sprites: "sprites"
+	}
+})
+define("main",["sprites/Sprite","sprites/MovableSprite","sprites/Spritesheet","Conductor"],function (Sprite,MovableSprite,Spritesheet,Conductor){
+	spritesheet = new Spritesheet("warrior1")
+	testSprite = new MovableSprite(50,50,"sprite-container-1",spritesheet)
+	Conductor.addSprite(testSprite)
+	
+	spritesheet = new Spritesheet("warrior2")
+	testSprite2 = new MovableSprite(50,50,"sprite-container-2",spritesheet)
+	Conductor.addSprite(testSprite2)
+	
+	testSprite.spawn(10,10)
+	testSprite2.spawn(300,300)
+	Conductor.renderAllSprites()
 });
+
+
 
 
 
---
+++ Static/Beau/scripts/sprites/MovableSprite.js
@@ -0,0 +1,103 @@
+define("sprites/MovableSprite",["sprites/Sprite","Conductor"],function(Sprite,Conductor){
+	
+	return class MovableSprite extends Sprite{
+		constructor(width,height,containerId,spritesheet){
+			super(width,height,containerId,spritesheet)
+			this.movement = []
+		}
+		moveHeld(dir,mod){
+			var me = this
+			me.movement[dir] = setInterval(function(){
+				me.move(dir,mod)
+			},frameRate)
+		}
+		stopMove(dir){
+			clearInterval(this.movement[dir])
+		}
+		setFacing(dir){
+			this.spriteSheet.prevRow = this.spriteSheet.row
+			this.spriteSheet.row = this.spriteSheet.facingList[dir]
+		}
+		move(dir,mod){
+			var revert = {xOld:this.x,yOld:this.y}
+			if (dir==this.spriteSheet.facingDict[0]){
+				var y = this.y+mod
+				if ( y < bgHeight ){
+					this.setYPos(y)
+				}
+			}
+			else if (dir==this.spriteSheet.facingDict[1]){
+				var y = this.y-mod
+				if ( y > 0 ){
+					this.setYPos(y)
+				}
+			}
+			else if (dir==this.spriteSheet.facingDict[2]){
+				var x = this.x-mod
+				if (x > 0){
+					this.setXPos(x)
+				}
+			}
+			else if (dir==this.spriteSheet.facingDict[3]){
+				var x = this.x+mod
+				if (x < bgWidth){
+					this.setXPos(x)
+				}
+			}
+			this.calcExtrema()
+			if (Conductor.hasCollision(this)){
+				this.setXPos(revert.xOld)
+				this.setYPos(revert.yOld)
+			}
+			
+		}
+		handleKeyPress(){
+			var me = this
+			
+			me.keySet = new Set()
+			$(document).keydown(function(event){
+				var key = String.fromCharCode(event.which)
+				if (key in me.spriteSheet.facingList){
+					if (!me.keySet.has(key)){
+						me.keySet.add(key)
+						me.setFacing(key)
+						me.render()
+						me.animate()
+						me.moveHeld(key,10)
+					}
+				}
+				
+				console.log("Press",key)
+				
+				
+			})
+			$(document).keyup(function(event){
+				var key = String.fromCharCode(event.which)
+				if (key in me.spriteSheet.facingList){
+					if (me.keySet.has(key)){
+						
+						me.stopAnimate()
+						me.stopMove(key)
+					}
+					me.keySet.delete(key)
+					if (me.keySet.size > 0){
+						var otherKey = me.keySet.values().next().value
+						me.setFacing(otherKey)
+						me.render()
+						me.animate()
+					}
+				}
+				
+				console.log("Release",key)
+				
+			})
+		}
+		spawn(x,y){
+			this.teleport(x,y)
+			this.loadImage()
+			this.handleKeyPress()
+			
+		}
+	}
+	
+})
