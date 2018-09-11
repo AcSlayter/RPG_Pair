1b299efc62b76dbb1c569a4666f883ae2e0f9fdc
AcSlayter <aaronbcain@gmail.com>
2018-09-10 22:03:52 -0700

    Adding KeyPress Stuff

--- Static/js/router.js
+++ Static/js/router.js
@@ -1,9 +1,24 @@
 // Filename: router.js
 
-define(['jquery'], function($){
+define(['jquery','helpers/KeyPress'], function($,KeyPress){
 
   var initialize = function(){
         console.log("Enter Router.js");
+
+        var current_page = window.location.pathname;
+        console.log(current_page);
+
+        switch(current_page){
+            case "/" :
+                console.log("something");
+                break;
+            case "/index.html" :
+                console.log("something 2 ");
+                break;
+            default:
+                console.log("fail");
+        }
+
   };
   return {
     initialize: initialize
